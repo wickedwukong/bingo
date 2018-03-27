4098ad21d9cbab52981c2521ada9cfacb366ef69
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2018-03-27 22:37:11 +0100

    use Type alias

--- Bingo.elm
+++ Bingo.elm
@@ -3,19 +3,37 @@ module Bingo exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
+type alias Entry =
+  { id : Int
+  , phrase : String
+  , points : Int
+  , marked : Bool
+  }
+
+type alias Model =
+  { name : String
+  , gameNumber : Int
+  , entries : List Entry
+}
+
 -- palyerInfo name gameNumber =
   -- name ++ " - Game #" ++ (toString gameNumber)
 
 -- MODEL
+initialModel : Model
 initialModel =
   {name =  "Xuemin"
   ,gameNumber =  3
   ,entries =  initialEntries
  }
 
+
+initialEntries : List Entry
 initialEntries =
-  [ {id = 1, phrase = "Future proof", points = 100, marked = False }
-  , {id = 2, phrase = "Doing Agile", points = 200, marked = False }
+  [ Entry 1 "Future proof" 100 False
+  , Entry 2 "Doing Agile" 200 False
+  , Entry 3 "In the cloud" 300 False
+  , Entry 4 "Rock star Ninja" 300 False
   ]
 
 
@@ -46,7 +64,7 @@ viewFooter =
          [text "Powered by Elm"]
       ]
 
--- view : Html msg
+view : Model -> Html msg
 view model =
   div [class "content"]
       [ viewHeader "BUZZWORD BINGO"
