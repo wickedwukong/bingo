127311a6bd7352f31f93bea5ace9c4dbeabae7cd
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2018-04-10 22:17:43 +0100

    adding points up by using List.filter, List.map, List.sum and |>

--- Bingo.elm
+++ Bingo.elm
@@ -96,12 +96,28 @@ viewEntryList entries =
   in
   ul [] listOfEntries
 
+sumMarkedPoints: List Entry -> Int
+sumMarkedPoints entries =
+  entries
+    |> List.filter .marked
+    |> List.map .points
+    |> List.sum
+
+viewScore: Int -> Html Msg
+viewScore sum =
+  div
+     [ class "score"]
+     [ span [class "label"] [text "Score"]
+     , span [class "value"] [text (toString sum)]
+     ]
+
 view : Model -> Html Msg
 view model =
   div [class "content"]
       [ viewHeader "BUZZWORD BINGO"
       , viewPlayer model.name model.gameNumber
       , viewEntryList model.entries
+      , viewScore (sumMarkedPoints model.entries)
       , div [class "button-group"]
             [button [onClick NewGame] [text "New Game"]]
       , div [class "debug"] [text (toString model)]
